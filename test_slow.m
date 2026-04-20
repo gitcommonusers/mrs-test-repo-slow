359a172f4791b0f46fede4a9ef8207244abac272
gitcommonusers <128743513+gitcommonusers@users.noreply.github.com>
2026-04-20 16:28:45 -0400

    Update test_slow.m

--- test_slow.m
+++ test_slow.m
@@ -2,7 +2,7 @@ classdef test_slow < matlab.unittest.TestCase
     methods(Test)
         function testThatTakesTooLong(testCase)
             fprintf('Starting a slow test that will pause for 30 seconds...\n');
-            pause(30);
+            pause(60);
             fprintf('Pause finished. This line should not be reached if timeout works.\n');
             testCase.verifyTrue(true);
         end
